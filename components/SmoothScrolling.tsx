14c7253c42e562c0e5fd55a40255a35f39755b5c
Naitik-Upadhayay-27 <upadhayaynaitik2712@gmail.com>
2025-08-21 10:51:52 -0700

    commit

--- components/SmoothScrolling.tsx
+++ components/SmoothScrolling.tsx
@@ -8,7 +8,6 @@ export default function SmoothScrolling() {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smooth: true,
       mouseMultiplier: 3,
       smoothTouch: false,
       touchMultiplier: 10,
